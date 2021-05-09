a94b998587efa1d96e5a076ac1d071273913e859
Ginyto <agianlucca@gmail.com>
2021-05-09 18:50:03 +0200

    classe Flashcard init

--- Js/Flashcards.js
+++ Js/Flashcards.js
@@ -3,13 +3,45 @@
  * @param recto face verso de la carte
  * @param verso face verso de la carte
  */
-class Flashcards{
-    constructor(recto, verso) {
-        this.recto = recto
-        this.verso = verso
+class Flashcards {
+	constructor(recto, verso, lvl, ) {
+		this.recto = recto;
+		this.verso = verso;
+		this.lvl = lvl;
+    }
+    
+    getLvl() {
+        return this.niveaux;
     }
 
-    
+    setLvl(lvl) {
+        this.lvl = lvl;
+    }
 
-    
+	getRecto() {
+		return this.recto;
+	}
+
+	getVerso() {
+		return this.verso;
+	}
+
+	setRecto(recto) {
+		this.recto = recto;
+	}
+
+	setVerso(verso) {
+		this.recto = verso;
+	}
+
+	fliprecto() {
+		this.recto.hidden = true;
+		this.verso.hidden = false;
+	}
+
+	flipverso() {
+		this.verso.hidden = true;
+		this.recto.hidden = false;
+	}
 }
+
