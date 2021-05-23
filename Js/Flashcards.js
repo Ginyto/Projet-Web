c3e079f3ff07aa3e509ffd9f02cdf405ce78c849
Ginyto <agianlucca@gmail.com>
2021-05-23 17:48:25 +0200

    ajout de la barre de vie, mode rpg

--- Js/Flashcards.js
+++ Js/Flashcards.js
@@ -1,35 +1,42 @@
+const flashcard = document.querySelector(".flashcard");
+var rface = document.getElementById("recto");
+var vface = document.getElementById("verso");
+var count = 0;
+var combien = document.getElementById("compteur");
+var bdv = document.getElementById("ko");
+var heal = 100;
+
+
 /**
  * Classe Flashcards qui a pour paramètre recto et verso
  * @param recto face verso de la carte
  * @param verso face verso de la carte
+ * @param check correcte ou incorecte
  */
 class Flashcards {
-	constructor(recto, verso, lvl, correct) {
-		this.recto = document.getElementById(recto);
-        this.verso = document.getElementById(verso);
-
-        this.crecto = recto.innerText;
-        this.cverso = verso.innerText;
-        
-        this.lvl = lvl;
-        this.correct = correct
+	constructor(recto, verso, check) {
+        this.recto = recto;
+        this.verso = verso;
+        this.check = check;
     }
 }
 
-
-
 function flip() {
-    const flashcard = document.querySelector('.flashcard');
     flashcard.style.transform = "rotateY(180deg)";
     console.log("Flip");
+    heal -= 2;
+    bdv.style.width = heal + "%";
+    console.log(heal);
+    
 }
 
 function flop() {
-	const flashcard = document.querySelector(".flashcard");
 	flashcard.style.transform = "rotateY(0deg)";
-	console.log("Flop");
+    console.log("Flop");
+    count++;
+    console.log(combien.textContent);
+    combien.textContent = count;
 }
-
 
 
 
